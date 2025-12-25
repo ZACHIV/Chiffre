ce2907fce4e9f80670f0d666b722e838b3b3fa93
zachmacmini <zack926done@gmail.com>
2025-12-25 22:34:54 +0800

    Initial Commit

---
+++ ChiffreTests/ChiffreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChiffreTests.swift
+//  ChiffreTests
+//
+//  Created by zachmacmini on 2025/12/25.
+//
+
+import Testing
+@testable import Chiffre
+
+struct ChiffreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
